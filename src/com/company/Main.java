ce0e230074d35b0a4fe509d766ba3e0e81a8aee9
AUdenkov <92884686+AUdenkov@users.noreply.github.com>
2021-10-30 15:43:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import com.company.vector.Operation;
+import com.company.vector.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Vector vector1 = new Vector(3, 6, 4);
+//        Vector vector2 = new Vector(12, 8, 7);
+        Operation operation = new Operation();
+//        double scalarMultiplication = operation.scalarMultiplication(vector1, vector2);
+//        System.out.println(scalarMultiplication);
+//        double sizeVector = operation.sizeVector(vector1);
+//        System.out.println(sizeVector);
+//        double[] vectorMultiplication = operation.vectorMultiplication(vector1, vector2);
+//        for (int i = 0; i < vectorMultiplication.length; i++) {
+//            System.out.print(" "+vectorMultiplication[i]+",");
+//        }
+//        System.out.println();
+//        double [] sumVector = operation.sumVector(vector1,vector2);
+//        for (int i = 0; i < sumVector.length; i++) {
+//            System.out.print(" "+ sumVector[i]+",");
+//        }
+//        System.out.println();
+//        double [] subtractionVector=operation.subtractionVector(vector1,vector2);
+//        for (int i = 0; i < subtractionVector.length; i++) {
+//            System.out.print(" "+ subtractionVector[i]+",");
+//        }
+//        System.out.println();
+//        double injection = operation.injection(vector1,vector2);
+//        System.out.println("injection " + injection);
+        operation.arrayVector(2);
+//        for (int i=0;i<chek.length;i++){
+//            System.out.println(chek[i]);
+//        }
+
+    }
+}
